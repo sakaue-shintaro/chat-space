d60698dbe88cdac1ab4d3150e838e948f2fd9dac
sakaue-shintaro <blues_feeling_0184_1115@yahoo.co.jp>
2019-12-16 09:44:11 +0900

    message_function

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,7 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
-  validates :content, presence: true, unless: :image?
+  validates :content,presence: true, unless: :image?
+  # validates :image,presence: true, unless: :content?
+  mount_uploader :image, ImageUploader
 end
