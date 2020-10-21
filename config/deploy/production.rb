2c7c0d102877df50cfb4b101d0065ce8d0e7f321
sakaue-shintaro <58502914+sakaue-shintaro@users.noreply.github.com>
2020-10-21 15:58:59 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '3.131.116.196', user: 'ec2-user', roles: %w{app db web}
+server '3.131.116.196', user: 'sakaue', roles: %w{app db web}
 
 
 # role-based syntax
@@ -42,9 +42,9 @@ server '3.131.116.196', user: 'ec2-user', roles: %w{app db web}
 # Global options
 # --------------
 set :ssh_options, {
-keys: keys: '~/.ssh/first_aws_rsa'
-forward_agent: false,
-auth_methods: %w(password)
+keys: '~/.ssh/first_aws_rsa'
+# forward_agent: false,
+# auth_methods: %w(password)
 }
 #
 # The server-based syntax can be used to override options:
