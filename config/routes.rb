e1b7eb38ca4c37b3da1cba8df21613d5d8436c51
sakaue-shintaro <58502914+sakaue-shintaro@users.noreply.github.com>
2020-11-02 14:06:35 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
-  resources :users,only:[:index,:edit,:update]
+  resources :users,only:[:index,:edit,:update,]
+    get '/users/sign_out' => 'devise/sessions#destroy'
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
     namespace :api do
