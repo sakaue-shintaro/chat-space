a90ca442238819bf765488ee3a94074ba3879f1f
takeshi-shiihara <shi4490@icloud.com>
2021-02-19 17:54:55 +0900

    スマホ画面途中

--- app/assets/javascripts/x-scroll.js
+++
@@ -1,4 +0,0 @@
-$(".side-ber__groups__group__name").on('click', function(){
-  $(".wrapper-overflow").removeClass("wrapper-overflow")
-  $("body").animate({scrollLeft: $("body").scrollLeft()+100,},300)
-})
